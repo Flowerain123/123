744c2401c6eba85a967e3880b3931bf2fc61f64e
Flowerain123 <1497550913@qq.com>
2024-04-04 22:57:29 +0800

    1.0

---
+++ src/come/java/test/Main.java
@@ -0,0 +1,11 @@
+package come.java.test;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(() -> new StartWin().setVisible(true));
+//        Win win = new Win();
+    }
+
+}
